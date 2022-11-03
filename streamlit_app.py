6c30ccc9c6234d8e1f700b15277251155d462ed4
Carlos Adrian Hernandez <116116271+CAhZ12@users.noreply.github.com>
2022-11-03 13:55:03 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,7 @@ streamlit.dataframe(my_data_rows)
 
 #add_my_fruit = 'Avocado','Strawberries','Kiwi','jackfruit'
 add_my_fruit = streamlit.multiselect ("What fruit would you like to add?", ['jackfruit'])
-streamlit.text("Thanks for adding")
-streamlit.dataframe(add_my_fruit) 
+streamlit.write('Thanks for adding',add_my_fruit) 
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
